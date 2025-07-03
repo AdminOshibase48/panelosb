76c0a10f9b78a6809ab1f4050f7943eb365f3ec1
AdminOshibase48 <oshibase48.ofc@gmail.com>
2025-07-04 04:04:52 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,3 @@
-const { createClient } = supabase;
-
-const supabaseClient = createClient(
-  "https://nuvfnnxuxtthbryesliu.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51dmZubnh1eHR0aGJyeWVzbGl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1Mzk1NTYsImV4cCI6MjA2NzExNTU1Nn0.Om73FRiKvdlWpZS-hIXCIKznjkQ3A3X3k1n2IzGzRes"
-);
+const supabaseUrl = "https://nuvfnnxuxtthbryesliu.supabase.co";
+const supabaseKey = "YeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51dmZubnh1eHR0aGJyeWVzbGl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1Mzk1NTYsImV4cCI6MjA2NzExNTU1Nn0.Om73FRiKvdlWpZS-hIXCIKznjkQ3A3X3k1n2IzGzRes; // Ganti ini dari Supabase
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
