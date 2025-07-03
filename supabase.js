33ba7534a5577fedca6e7226cac7ebf0e93aad6c
AdminOshibase48 <oshibase48.ofc@gmail.com>
2025-07-04 03:42:16 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,6 @@
-const supabase = supabase.createClient(
-  "https://nuvfnnxuxtthbryesliu.supabase.co", 
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51dmZubnh1eHR0aGJyeWVzbGl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1Mzk1NTYsImV4cCI6MjA2NzExNTU1Nn0.Om73FRiKvdlWpZS-hIXCIKznjkQ3A3X3k1n2IzGzRes" // Ganti ini dengan anon key milikmu
+const { createClient } = supabase;
+
+const supabaseClient = createClient(
+  "https://nuvfnnxuxtthbryesliu.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51dmZubnh1eHR0aGJyeWVzbGl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1Mzk1NTYsImV4cCI6MjA2NzExNTU1Nn0.Om73FRiKvdlWpZS-hIXCIKznjkQ3A3X3k1n2IzGzRes"
 );
