cc4af94c0f2707438cde0bbf20e03efe8b6157dd
AdminOshibase48 <oshibase48.ofc@gmail.com>
2025-07-04 14:00:28 +0700

    Create login.js

---
+++ assets/js/login.js
@@ -0,0 +1,18 @@
+import { supabase } from './supabaseClient.js'
+
+document.getElementById('loginBtn')?.addEventListener('click', async () => {
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  })
+
+  if (error) {
+    alert('Login gagal: ' + error.message)
+  } else {
+    alert('Login berhasil!')
+    window.location.href = 'dashboard.html'
+  }
+})
