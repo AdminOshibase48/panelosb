e539e78d787239a6fa265d92a7d2285dff51a4c9
AdminOshibase48 <oshibase48.ofc@gmail.com>
2025-07-04 13:54:49 +0700

    Create supabaseClient.js

---
+++ assets/js/supabaseClient.js
@@ -0,0 +1,7 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+// Ganti dengan punyamu:
+const SUPABASE_URL = 'https://nuvfnnxuxtthbryesliu.supabase.co'   // <-- Ambil dari "Project URL"
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51dmZubnh1eHR0aGJyeWVzbGl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1Mzk1NTYsImV4cCI6MjA2NzExNTU1Nn0.Om73FRiKvdlWpZS-hIXCIKznjkQ3A3X3k1n2IzGzRes'     // <-- Gunakan anon public key
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
