bf7efef9284c1fe27f555b0de763faca343eeb3f
AdminOshibase48 <oshibase48.ofc@gmail.com>
2025-07-05 11:03:28 +0700

    Update galeri-admin.js

--- assets/js/galeri-admin.js
+++ assets/js/galeri-admin.js
@@ -2,7 +2,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 const supabase = createClient(
   'https://nuvfnnxuxtthbryesliu.supabase.co',
-  'public-anon-key-kamu' // GANTI!
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51dmZubnh1eHR0aGJyeWVzbGl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1Mzk1NTYsImV4cCI6MjA2NzExNTU1Nn0.Om73FRiKvdlWpZS-hIXCIKznjkQ3A3X3k1n2IzGzRes' // GANTI!
 );
 
 const gallery = document.getElementById('gallery-grid');
